d58d6d7dbfec79b3ea581f210f678310dfa5108f
Jack Fouch <fouchjw@alma.edu>
2021-01-24 15:51:42 -0500

    added development message

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar sticky-top navbar-expand-lg mb-5">
 
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Jack Fouch</a>
+      <a class="navbar-brand" href="#">Jack Fouch (website still being developed)</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
